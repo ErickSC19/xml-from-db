8d95559e3f11d9767a9afa163a4029ab18fab70a
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-10-01 01:45:23 -0600

    xml structure fix

--- xmlMethods.js
+++ xmlMethods.js
@@ -2,8 +2,10 @@ import builder from 'xmlbuilder'
 import fs from 'fs';
 
 export function convertToXML(data) {
-  const product = builder.create('product');
-  product.ele('id', data.id);
+  const product = builder.create('products', {
+    version: "1.0", 
+    encoding: "UTF-8"})
+    .ele('product', {id: data.id});
   product.ele('name', data.name);
   product.ele('price', data.price);
   product.ele('count', data.count);
