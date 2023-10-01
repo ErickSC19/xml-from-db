3eb3310057f2942d786fbdc3899d7cbf1e5e0755
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-09-30 19:15:00 -0600

    fix

--- xmlMethods.js
+++ xmlMethods.js
@@ -2,15 +2,14 @@ import builder from 'xmlbuilder'
 import fs from 'fs';
 
 export function convertToXML(data) {
-  const root = builder.create('root')
-  .ele('id', data.id)
-  .ele('name', data.name)
-  .ele('price', data.price)
-  .ele('count', data.count);
+  const product = builder.create('product');
+  product.ele('id', data.id);
+  product.ele('name', data.name);
+  product.ele('price', data.price);
+  product.ele('count', data.count);
   
-  return root.end({ pretty: true });
+  return product.end({ pretty: true });
 }
-
 
 export function writeXMLToFile(xmlData, filePath) {
   fs.writeFileSync(filePath, xmlData, 'utf-8');
